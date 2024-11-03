d537d181b184b56a3bb2a902061d3d53d54614a6
Nitin Khare <nknitinkhare13@gmail.com>
2024-11-03 18:45:18 +0530

    basic negative test case added

--- app.js
+++ app.js
@@ -3,6 +3,18 @@ export class Calculator {
         if(num === ""){
             return 0;
         }
-        return num;
+        if(!isNaN(num)){
+            return parseFloat(num);
+        }
+        const nums = num.split(",");
+        let result = 0;
+        for(let i=0; i<nums.length; i++){
+            if(isNaN(nums[i])){
+                throw new Error(`Invalid Number Found : ${nums[i].trim()}`)
+            }
+            result += parseFloat(nums[i].trim());
+
+        }
+        return result;
     }
 }
--- test/app.spec.js
+++ test/app.spec.js
@@ -7,14 +7,28 @@ describe('Testing Calculator functionalities ', ()=>{
     beforeEach(() => {
         calc = new Calculator();
     });
-    
+
     //Input: “”, Output: 0
-    it("should return 0 on empty string ",()=>{
+    it("it should return 0 on empty string ",()=>{
         expect(calc.add(0)).to.be.equal(0);
     })
 
-    it("should add", ()=>{
+    it("it should add", ()=>{
         expect(calc.add(0)).to.equal(0);
     })
+    //Input: “1,5”, Output: 6
+    it("it should return sum for two numbers", () => {
+        expect(calc.add("1,5")).to.equal(6);
+    });
+
+    //Input: “1, 5”, Output: 6
+    it("it should return sum for two numbers handling trailing spaces", () => {
+        expect(calc.add("1, 9 ")).to.equal(10);
+    });
+
+    it("it should fail for invalid numbers", () => {
+        expect(() => calc.add("-1, 2.f")).to.throw('Invalid Number Found : 2.f');
+
+    });
 
 })
