8059b86a356d7c427b528b82803660ff1e011d72
Nitin Khare <nknitinkhare13@gmail.com>
2024-11-03 18:33:16 +0530

    return 0 in case of empty string test case added

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 export class Calculator {
     add(num) {
+        if(num === ""){
+            return 0;
+        }
         return num;
     }
 }
--- test/app.spec.js
+++ test/app.spec.js
@@ -7,6 +7,11 @@ describe('Testing Calculator functionalities ', ()=>{
     beforeEach(() => {
         calc = new Calculator();
     });
+    
+    //Input: “”, Output: 0
+    it("should return 0 on empty string ",()=>{
+        expect(calc.add(0)).to.be.equal(0);
+    })
 
     it("should add", ()=>{
         expect(calc.add(0)).to.equal(0);
